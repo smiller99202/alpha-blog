08ad7bdba65b86c0982cd54ab9f21fd0399675df
smiller99202 <smiller99202@gmail.com>
2018-01-10 20:28:25 +0000

    Added Article Create and Show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,13 +6,21 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save    
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "The article saved successfully"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])  
   end
 
   private
   def article_params
-    params.requre(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description)
   end
 
 end
